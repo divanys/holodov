9e77d11336839e029c00f8713631322ece856672
divan <hlinakcurbag@gmail.com>
2023-09-29 13:46:16 +0300

    указатели

---
+++ pointers.cpp
@@ -0,0 +1,35 @@
+//
+// Created by divan on 9/29/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int sum_by_value(int value) {
+    value += value;
+    return value;
+}
+
+int sum_by_reference(int &reference) {  // если const, то запрещаем менять reference
+    reference += reference;
+    return reference;
+}
+
+int sum_by_pointer(int *ptrvalue) {
+    *ptrvalue += *ptrvalue;
+    return *ptrvalue;
+}
+
+int main() {
+    int value = 10;
+    cout << "sum_by_value = " << sum_by_value(value) << endl;
+    cout << "value = " << value << endl;
+    cout << "sum_by_reference = " << sum_by_reference(value) << endl;
+    cout << "value = " << value << endl;
+    cout << "sum_by_pointer = " << sum_by_pointer(&value) << endl;  // если указатель терпит изменение, то
+    cout << "value = " << value << endl;
+
+    return 0;
+}
+
