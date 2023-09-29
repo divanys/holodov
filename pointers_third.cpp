7f42beb63560b4ee3b132896ddb6f08590abbc88
divan <hlinakcurbag@gmail.com>
2023-09-29 14:08:01 +0300

    указатели negst ,kby

--- pointers_third.cpp
+++ pointers_third.cpp
@@ -11,8 +11,8 @@ int main() {
     int var1 = 123, var2 = 99;
     int *ptrvar1 = &var1, *ptrvar2 = &var2;
 
-    cout << "&var1 = " << &var1 << endl;
-    cout << "&var2 = " << &var2 << endl;
+    cout << "&var1 = " << var1 << endl;
+    cout << "&var2 = " << var2 << endl;
     cout << "ptrvar1 = " << ptrvar1 << endl;
     cout << "ptrvar2 = " << ptrvar2 << endl;
 
