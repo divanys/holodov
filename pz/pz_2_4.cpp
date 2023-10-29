fb9d0a3c59f2d46f08cba78d5c5fdd1dd18e577b
divan <hlinakcurbag@gmail.com>
2023-10-29 15:55:53 +0300

    pz 2 is done

--- pz/pz_2_4.cpp
+++ pz/pz_2_4.cpp
@@ -5,54 +5,59 @@
 // Затем упорядочить по алфавиту второй список методом «пузырька»
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
-// Функция для фильтрации фамилий и сохранения во втором списке
-void filterSurnames(const vector<string>& originalList, vector<string>& filteredList) {
-    for (const string& surname : originalList) {
-        if (surname.length() >= 2 && surname[1] == 'l') {
-            filteredList.push_back(surname);
-        }
-    }
-}
-
-// Функция для сортировки списка методом пузырька
-void bubbleSort(vector<string>& surnames) {
-    int size = surnames.size();
-    for (int i = 0; i < size - 1; i++) {
-        for (int j = 0; j < size - i - 1; j++) {
-            if (surnames[j] > surnames[j + 1]) {
-                swap(surnames[j], surnames[j + 1]);
-            }
-        }
-    }
-}
-
 int main() {
-    vector<string> originalList;
-    vector<string> filteredList;
-
     int n;
     cout << "Введите количество фамилий: ";
     cin >> n;
 
-    // Ввод фамилий сотрудников
+    string *surnames = new string[n];
+
+    cout << "Введите фамилии сотрудников:" << endl;
     for (int i = 0; i < n; i++) {
-        string surname;
-        cout << "Введите фамилию " << i + 1 << ": ";
-        cin >> surname;
-        originalList.push_back(surname);
+        cin >> surnames[i];
     }
 
-    filterSurnames(originalList, filteredList);
-    bubbleSort(filteredList);
+    string *filteredSurnames = new string[n];
+    int filteredCount = 0;
 
-    cout << "Фамилии, в которых вторая буква - 'l':\n";
-    for (const string& surname : filteredList) {
-        cout << surname << endl;
+    // Фильтрация фамилий
+    for (int i = 0; i < n; i++) {
+        string surname = surnames[i];
+
+        if (surname.size() >= 2) {
+            int lCount = 0;
+
+            for (char c: surname) {
+                if (c == 'l' || c == 'L') {
+                    lCount++;
+                }
+            }
+
+            if (lCount == 2) {
+                filteredSurnames[filteredCount] = surname;
+                filteredCount++;
+            }
+        }
     }
+
+    for (int i = 0; i < filteredCount - 1; i++) {
+        for (int j = 0; j < filteredCount - i - 1; j++) {
+            if (filteredSurnames[j] > filteredSurnames[j + 1]) {
+                swap(filteredSurnames[j], filteredSurnames[j + 1]);
+            }
+        }
+    }
+
+    cout << "Отфильтрованные и отсортированные фамилии:" << endl;
+    for (int i = 0; i < filteredCount; i++) {
+        cout << filteredSurnames[i] << endl;
+    }
+
+    delete[] surnames;
+    delete[] filteredSurnames;
 
     return 0;
 }
