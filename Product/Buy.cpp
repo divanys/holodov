13254e26ca33ceb938e6f40aaec32a66ad2df315
divan <hlinakcurbag@gmail.com>
2023-11-07 15:36:20 +0300

    рабочий

--- Product/Buy.cpp
+++ Product/Buy.cpp
@@ -2,32 +2,34 @@
 // Created by divan on 11/7/23.
 //
 
-#include "buy.h"
- 
-Buy::Buy() // конструктор
-    : sum(0), sum_price(0.0), sum_weight(0.0)
-{
-    // пустое тело конструктора
+#include "Buy.h"
+#include <iostream>
+
+using namespace std;
+
+Buy::Buy()// конструктор
+        : sum(0), sum_price(0.0), sum_weight(0.0) {
+// пустое тело конструктора
 }
- 
-void Buy::setSum(const int &value) // установить количество покупаемого товара
+
+void Buy::setSum(const int &value)// установить количество покупаемого товара
 {
-    sum = (value >= 0 ? value : 0);
-    sum_price = sum * getPrice(); // получаем стоимость всего товара
-    sum_weight = sum * getWeight(); // получаем общий вес покупаемого товара
+    sum = (value >= 0 ? value : 0);
+    sum_price = sum * getPrice();// получаем стоимость всего товара
+    sum_weight = sum * getWeight();// получаем общий вес покупаемого товара
 }
- 
-int Buy::getSum() const // возвратить количество покупаемого товара
+
+int Buy::getSum() const // возвратить количество покупаемого товара
 {
-    return sum;
+    return sum;
 }
- 
-double Buy::getSumPrice() const // возвратить цену за весь купленный товар
+
+double Buy::getSumPrice() const // возвратить цену за весь купленный товар
 {
-    return sum_price;
+    return sum_price;
 }
- 
-double Buy::getSumWeight() const // возвратить общий вес покупки
+
+double Buy::getSumWeight() const // возвратить общий вес покупки
 {
-    return sum_weight;
+    return sum_weight;
 }
--- Product/Buy.h
+++ Product/Buy.h
@@ -4,24 +4,24 @@
 
 #ifndef HOLODOV_BUY_H
 #define HOLODOV_BUY_H
-// заголовочный фай производного класса Buy для класса Product 
-#include "product.h"
- 
-class Buy : public Product // открытое наследование
+// заголовочный фай производного класса Buy для класса Product
+#include "Product.h"
+
+class Buy : public Product// открытое наследование
 {
 private:
-     int    sum; // количество покупаемого товара в шт.
-     double sum_price, // цена за весь купленный товар   
-            sum_weight; // общий вес
+    int sum;// количество покупаемого товара в шт.
+    double sum_price,// цена за весь купленный товар
+    sum_weight;// общий вес
 public:
-    Buy(); // конструктор
- 
-    void setSum(const int &); // установить количество покупаемого товара
-    int getSum() const; // возвратить количество покупаемого товара
- 
-    double getSumPrice() const; // возвратить цену за весь купленный товар
- 
-    double getSumWeight() const; // возвратить общий вес покупки
+    Buy();// конструктор
+
+    void setSum(const int &);// установить количество покупаемого товара
+    int getSum() const;// возвратить количество покупаемого товара
+
+    double getSumPrice() const;// возвратить цену за весь купленный товар
+
+    double getSumWeight() const;// возвратить общий вес покупки
 };
 
 #endif //HOLODOV_BUY_H
