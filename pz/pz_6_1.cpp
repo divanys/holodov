3cf922fe004b5abe5b8d9db3c182e766b93d1e7a
divan <hlinakcurbag@gmail.com>
2023-11-11 12:25:26 +0300

    допилила пз 1-6

--- pz/pz_6_1.cpp
+++ pz/pz_6_1.cpp
@@ -4,9 +4,9 @@
 // Разработка программ на языке С++ для обработки строк.
 // Пользователь вводит текст. Вывести исходный текст, заменив в нем слово <Pascal> на <C++>,
 // подсчитав их количество. Вычислить количество слов <компьютер>.
-
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -16,31 +16,36 @@ int main() {
     cout << "Введите входной текст для обработки: ";
     getline(cin, text);
 
-    const string PASKAL = "Pascal";
-    const string CIKRESTIKKRESTIK = "C++";
+    string PASCAL = "pascal";
+    const string CPP = "C++";
 
-    size_t index_find_word = text.find(PASKAL);
+    transform(text.begin(), text.end(), text.begin(), ::tolower); // Приведение к нижнему регистру
 
-    while (index_find_word < text.size()) {
-        text.erase(index_find_word, PASKAL.size());
-        text.insert(index_find_word, CIKRESTIKKRESTIK);
-        index_find_word = text.find(PASKAL);
+    size_t index_find_word = text.find(PASCAL);
+
+    while (index_find_word != string::npos) {
+        text.replace(index_find_word, PASCAL.size(), CPP);
+        index_find_word = text.find(PASCAL, index_find_word + CPP.size());
     }
 
-    const string KOMPUTER = "komputer";
+    string KOMPUTER = "komputer";
+
+    transform(text.begin(), text.end(), text.begin(), ::tolower);
+    transform(KOMPUTER.begin(), KOMPUTER.end(), KOMPUTER.begin(), ::tolower);
 
     index_find_word = text.find(KOMPUTER);
 
-    while (index_find_word < text.size()) {
-        text.erase(index_find_word, KOMPUTER.size());
-        index_find_word = text.find(KOMPUTER);
+    int count_komputer = 0;
+
+    while (index_find_word != string::npos) {
+        count_komputer++;
+        index_find_word = text.find(KOMPUTER, index_find_word + KOMPUTER.size());
     }
 
-    cout << "Текст после обработки: " << text;
+    cout << "Текст после обработки: " << text << endl;
+    cout << "Количество слов komputer: " << count_komputer << endl;
 
-    fflush(stdin);
-
-    cout << endl << endl << "Для завершения работы программы нажмите ENTER...";
+    cout << endl << "Для завершения работы программы нажмите ENTER...";
     cin.get();
 
     return 0;
